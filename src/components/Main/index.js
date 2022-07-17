1b35d420f7bbb14a4c0768456a461e1fbe420228
shabaaz27 <khanshabaaz05@gmail.com>
2022-07-17 16:32:08 +0530

    onload dispatch

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import {useDispatch,useSelector} from 'react-redux'
 import { addToCart, emptyCart, removeFromCart} from '../../redux/actions/action';
 import { productList } from '../../redux/actions/productActions';
@@ -10,12 +11,32 @@ function Main() {
 
   const id = Math.random() * 2 + 1
   const car = {type:"Fiat", model:"500", color:"white",id}
+useEffect(()=>{
+  dispatch(productList())
+},[])
+
   return (
     <>
         <section><button onClick={()=>dispatch(addToCart(car))}>Add to Cart</button></section>
         <section><button onClick={()=>dispatch(removeFromCart(cartDetails[0]?.id))}>Remove from Cart</button></section>
         <section><button onClick={()=>dispatch(emptyCart())}>Delete Cart</button></section>
         <section><button onClick={()=>dispatch(productList())}>get Product</button></section>
+        <section className='product-container'>
+          {
+            data && data.map((item)=><div key={item.id} className='product-item' style={{textAlign:"center",margin:"10px"}}>
+              <img src={item.photo} alt="picture"/>
+              <div>Name: {item.name}</div>
+              <div>Color: {item.color}</div>
+              <div>Brand: {item.brand}</div>
+              <div>Price: {item.price}</div>
+              <div>
+                <button onClick={()=>dispatch(addToCart(item))}>Add To Cart</button>
+                <button onClick={()=>dispatch(removeFromCart(item.id))}>Remove From Cart</button>
+              </div>
+            </div>
+          )
+          }
+        </section>
     </>
   );
 }
