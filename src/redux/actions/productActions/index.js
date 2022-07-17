0857fe8d8791af02bf0304e5e7c766933c489656
shabaaz27 <khanshabaaz05@gmail.com>
2022-07-17 12:45:22 +0530

    configure saga

--- src/redux/actions/productActions/index.js
+++ src/redux/actions/productActions/index.js
@@ -1,13 +1,13 @@
-import { PRODUCT_LIST } from "../../constants"
+import { PRODUCT_LIST, SET_PRODUCT_LIST } from "../../constants"
 
 
-export const productList = async ()=>{
-    let data = await fetch(`https://jsonplaceholder.typicode.com/todos/1`)
-    data = await data.json()
+export const productList = ()=>{
+    // let data = await fetch(`https://jsonplaceholder.typicode.com/todos/1`)
+    // data = await data.json()
 
-    console.warn("action called","JJJEJNKJNJK",data)
+    // console.warn("action called","JJJEJNKJNJK",data)
     return {
         type:PRODUCT_LIST,
-        data:"hello"
     }
 }
+
