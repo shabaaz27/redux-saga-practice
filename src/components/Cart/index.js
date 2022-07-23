3fe2729dab8ea8a43d9c7ded5b654eff4fffd4a4
shabaaz27 <khanshabaaz05@gmail.com>
2022-07-23 22:33:53 +0530

    cart table added with amount

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -1,10 +1,44 @@
 
-import {Routes,Route} from 'react-router-dom'
+import {Routes,Route} from 'react-router-dom';
+
+import {useSelector} from 'react-redux'
 
 function Cart() {
+  const cartDetails = useSelector(state=>state?.cartData)
+  const amount = cartDetails.length && cartDetails.map(item=>item.price).reduce((prev,next)=>prev+next,0)
+  console.log(amount)
   return (
     <div className="Cart">
-            <h1>Cart Page</h1>
+           <h1>Cart Page</h1>
+      <div className="cart-container">
+      
+        <table>
+          <tr>
+              <td>Name</td>
+              <td>Color</td>
+              <td>Price</td>
+              <td>Brand</td>
+              <td>catgory</td>
+          </tr>
+          {
+            cartDetails?.map((item)=> 
+                <tr key={item.id}>
+                  <td>{item.name}</td>
+                  <td>{item.color}</td>
+                  <td>{item.price}</td>
+                  <td>{item.brand}</td>
+                  <td>{item.category}</td>
+                </tr>
+            )
+          }
+        </table>
+        <div className="price-details">
+            <div className='adjust-amount'><span>Amount</span><span>{amount}</span></div>
+            <div className='adjust-amount'><span>Discount</span><span>{amount/10}</span></div>
+            <div className='adjust-amount'><span>Tax</span><span>0000</span></div>
+            <div className='adjust-amount'><span>Total</span><span>{amount-amount/10}</span></div>
+        </div>
+      </div>      
     </div>
   );
 }
