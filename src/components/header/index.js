cb3030b8ff79e133f83a84883e0f68d6d0e63779
shabaaz27 <khanshabaaz05@gmail.com>
2022-07-23 22:34:29 +0530

    Header with input text added

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,12 +1,22 @@
 import {cart} from '../../assets'
-import { useSelector } from 'react-redux'
+import { useSelector,useDispatch } from 'react-redux'
+import { productSearch } from '../../redux/actions/productActions'
 import {Link} from 'react-router-dom'
+
 const Header = ()=>{
+    const dispatch = useDispatch()
     const result = useSelector((state)=>state?.cartData)
     console.log("result==>",result)
     return(
             <div className="header">
+                <Link to="/">
+                <h1 style={{float:"left",paddingLeft:"20px"}}>Logo</h1>
+                </Link>
+                <input type="text" onChange={(e)=>dispatch(productSearch(e.target.value))} placeholder='Search Product'/>
                 <Link to="/cart">
+                    
+                       
+                  
                 <div className="cart-div">
                     <span style={{fontSize:"15px"}}>{result.length}</span>
                     <img src={cart} alt="cart"/>
